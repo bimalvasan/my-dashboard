c9e1bcc4365084929acd96ad8e501e4719569d5b
Bimal <bimalvasan@gmail.com>
2015-09-26 21:56:49 -0500

    Include Market types

--- www/app/segment/market.controller.js
+++ www/app/segment/market.controller.js
@@ -3,7 +3,17 @@
     angular.module('main').controller('MarketCtrl', MarketCtrl);
 
     function MarketCtrl() {
-        var mkt = this;
-        mkt.markets = ['USA', 'UK', 'Mexico', 'Canada'];
+        var vm = this;
+        
+        vm.markets = [{
+            'key': 'US',
+            'icon': '../img/Flag_US.png'
+        }, {
+            'key': 'UK',
+            'icon': '../img/Flag_UK.png'
+        }, {
+            'key': 'Canada',
+            'icon': '../img/Flag_Canada.png'
+        }];
     };
 })();
